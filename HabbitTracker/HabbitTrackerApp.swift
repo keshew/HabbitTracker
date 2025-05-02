18b1e9d2588298c5fc4c71f558e9e591713d9e12
keshew <artyom.korotckov2017@yandex.ru>
2025-05-03 00:31:09 +0500

    Initial Commit

---
+++ HabbitTracker/HabbitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabbitTrackerApp.swift
+//  HabbitTracker
+//
+//  Created by Артём Коротков on 03.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct HabbitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
